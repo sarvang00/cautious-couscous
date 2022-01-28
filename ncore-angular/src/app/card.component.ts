7e8c5b19bc1af021c801ba93cabf27d84d2a3509
Sarvang Dave <sarvang00@gmail.com>
2022-01-28 19:55:42 +0530

    And that's how you add a new component in same mod

---
+++ ncore-angular/src/app/card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "card-thingy",
+    templateUrl: "card.component.html",
+    styleUrls: ["card.component.css"]
+})
+export class CardComponent{}
