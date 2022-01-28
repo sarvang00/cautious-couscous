f00698b7775ee18857a935e669e19594e04ec368
Sarvang Dave <sarvang00@gmail.com>
2022-01-28 19:43:42 +0530

    And now it is modular - like it's meant to be

--- ncore-angular/src/app/app.component.ts
+++ ncore-angular/src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "my-app",
-    template: `<h1>Hey Angular</h1>`,
-    styles: [`
-    h1 {
-        background-color: #fff;
-        text-align: center;
-    }
-    `],
+    templateUrl: "app.component.html",
+    styleUrls: ["app.component.css"],
 })
 export class AppComponent {}
