1bc2e0f084a05ed935e055dd3388406951429175
Sarvang Dave <sarvang00@gmail.com>
2022-01-30 17:09:20 +0530

    Completed task to choose words or country names

--- new-word-generator/src/app/app.component.ts
+++ new-word-generator/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import arrayWords from "../utils/words"
+import countryNames from "../utils/countries"
 
 @Component({
   selector: 'app-root',
@@ -19,6 +20,17 @@ export class AppComponent {
   
   generate() {
     this.words = arrayWords.slice(0, this.limit).join(", ");
+  }
+
+  getRandCountry() {
+    for (let i:number =0; i < this.limit; i++) {
+      if (i==0) {
+        this.words = countryNames[Math.floor(Math.random()*226) + 1];
+      }
+      else{
+        this.words = this.words + ", " + countryNames[Math.floor(Math.random()*226) + 1];
+      }
+    }
   }
 
 }
