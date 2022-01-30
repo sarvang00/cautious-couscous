ac9c7cc2f3013396052edd122c974bc306fd985a
Sarvang Dave <sarvang00@gmail.com>
2022-01-30 16:18:21 +0530

    Interacting Slider, words  and values

--- new-word-generator/src/app/app.component.ts
+++ new-word-generator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-word-generator';
+
+  words = '';
+  limit = 10;
+
+  handleSlideChange(newLimit: number) {
+    console.log(this.limit)
+    this.limit = newLimit;
+  }
+  
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(", ");
+  }
+
 }
