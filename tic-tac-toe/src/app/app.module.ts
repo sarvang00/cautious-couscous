b8e6b87be6749a86c73e7de7999efe8c69e8d1d2
Sarvang Dave <sarvang00@gmail.com>
2022-02-05 19:47:22 +0530

    Importing necessary modules

--- tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IconComponent } from './components/icon/icon.component';
 
+// Fontawesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// Toast
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent,
     IconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
