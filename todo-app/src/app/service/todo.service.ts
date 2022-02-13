c2c68990d064dbea8f4095540613dd8d93698c93
Sarvang Dave <sarvang00@gmail.com>
2022-02-13 20:40:00 +0530

    Wrote busniess logic of Service

--- todo-app/src/app/service/todo.service.ts
+++ todo-app/src/app/service/todo.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from '../model/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todos: Todo[] = new Array();
+  constructor() {
+    this.todos = [
+      {
+        id: '111',
+        title: "Learn C++",
+        isComplete: true,
+        date: new Date(),
+      },
+      {
+        id: '222',
+        title: "Learn Golang",
+        isComplete: true,
+        date: new Date(),
+      },
+      {
+        id: '333',
+        title: "Learn Angular",
+        isComplete: true,
+        date: new Date(),
+      },
+    ];
+   }
+  
+   getTodos() {
+     return of(this.todos)
+   }
+
+   addTodo(todo: Todo) {
+     this.todos.push(todo)
+   }
+
+   changeTodoStatus(todo: Todo) {
+     this.todos.map( singeTodo => {
+       if (singeTodo.id == todo.id) {
+         singeTodo.isComplete = !singeTodo.isComplete;
+       }
+     });
+   }
+
+   deleteTodo(todo: Todo) {
+     const indexOfTodo = this.todos.findIndex(
+       (currentObj) => currentObj.id === todo.id
+     );
+     this.todos.splice(indexOfTodo, 1);
+   }
 }
