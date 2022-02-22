aa6e6d602ecd1fde7e02d3d68c89f3b33eea38b8
Sarvang Dave <sarvang00@gmail.com>
2022-02-22 23:18:57 +0530

    Setting up component for reactive form backend

--- signup-reactive/src/app/app.component.ts
+++ signup-reactive/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { ReturnStatement } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PasswordChecker } from './custom-validators/password-checker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signup-reactive';
+  registerForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void { 
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      acceptTandC: [false, Validators.requiredTrue]
+    }, {
+      validators: PasswordChecker('password', 'confirmPassword')
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    console.table(this.registerForm.value);
+    console.table(this.registerForm);
+
+    if (this.registerForm.invalid) {
+      return;
+    } else {
+      alert("Success Signup!\n"+JSON.stringify(this.registerForm.value))
+    }
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
