7b89ce731b8a67e8f7e4626530f24438dbca7a0c
Sarvang Dave <sarvang00@gmail.com>
2022-02-22 23:24:59 +0530

    Module import fix

--- signup-reactive/src/app/app.module.ts
+++ signup-reactive/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
