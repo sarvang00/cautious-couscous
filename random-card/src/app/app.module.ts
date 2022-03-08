19c9ea5a7007d8dfa40035ab87ce6a0053b2f66a
Sarvang Dave <sarvang00@gmail.com>
2022-03-08 14:39:27 +0530

    adding modules necessary for the app

--- random-card/src/app/app.module.ts
+++ random-card/src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// HTTP module
+import { HttpClientModule } from "@angular/common/http"
+// Toast
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+// Fontawesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 
@@ -10,7 +18,11 @@ import { CardComponent } from './card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule, //required animations module
+    ToastrModule.forRoot(), //toastr module
+    FontAwesomeModule //fontawesome
   ],
   providers: [],
   bootstrap: [AppComponent]
