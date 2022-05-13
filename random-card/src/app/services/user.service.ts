d06fad7a3c49fed9006ed81a3c71c5f7254c3c18
Sarvang Dave <sarvang00@gmail.com>
2022-05-13 23:00:01 +0530

    card component accepts user; fixed subscribe-rxjs

--- random-card/src/app/services/user.service.ts
+++ random-card/src/app/services/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  // the api url
+  private url = "https://randomuser.me/api";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getUser(){
+    return this.http.get(this.url)
+  }
 }
