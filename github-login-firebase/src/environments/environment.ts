fc82f45b34748576db3897f87c93e456e010e2bb
Sarvang Dave <sarvang00@gmail.com>
2022-05-17 12:17:09 +0530

    Registered the app with firebase

--- github-login-firebase/src/environments/environment.ts
+++ github-login-firebase/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'github-login-firebase-a2e88',
+    appId: '1:412323938595:web:9081305ef1115c60594c96',
+    storageBucket: 'github-login-firebase-a2e88.appspot.com',
+    apiKey: 'AIzaSyDBX1hLFTZAjIIMT2jJNrj88tH65nAbxPk',
+    authDomain: 'github-login-firebase-a2e88.firebaseapp.com',
+    messagingSenderId: '412323938595',
+  },
   production: false,
   firebaseConfig : {
     apiKey: "AIzaSyDBX1hLFTZAjIIMT2jJNrj88tH65nAbxPk",
