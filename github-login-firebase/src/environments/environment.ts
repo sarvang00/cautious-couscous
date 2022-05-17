3892f4541e2ef83a1075dae77fde35b2c4f49ec3
Sarvang Dave <sarvang00@gmail.com>
2022-05-17 12:17:42 +0530

    We probably don't need this again

--- github-login-firebase/src/environments/environment.ts
+++ github-login-firebase/src/environments/environment.ts
@@ -11,15 +11,7 @@ export const environment = {
     authDomain: 'github-login-firebase-a2e88.firebaseapp.com',
     messagingSenderId: '412323938595',
   },
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyDBX1hLFTZAjIIMT2jJNrj88tH65nAbxPk",
-    authDomain: "github-login-firebase-a2e88.firebaseapp.com",
-    projectId: "github-login-firebase-a2e88",
-    storageBucket: "github-login-firebase-a2e88.appspot.com",
-    messagingSenderId: "412323938595",
-    appId: "1:412323938595:web:9081305ef1115c60594c96"
-  }
+  production: false
 };
 
 /*
