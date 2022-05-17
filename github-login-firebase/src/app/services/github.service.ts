4a37b7003aff8567c517f10b30627d64cd5c7a0d
Sarvang Dave <sarvang00@gmail.com>
2022-05-17 22:28:32 +0530

    Making github service

--- github-login-firebase/src/app/services/github.service.ts
+++ github-login-firebase/src/app/services/github.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails(userName: string) {
+    return this.http.get(`https://api.github.com/users/hiteshchoudhary/${userName}`)
+  }
+
+  getRepos(repoUrl: string) {
+    return this.http.get(repoUrl);
+  }
 }
