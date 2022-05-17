643b0faae77059f1c4ca1cb71601559eecfc1b80
Sarvang Dave <sarvang00@gmail.com>
2022-05-17 18:20:48 +0530

    Making some pages components

--- github-login-firebase/src/app/app.module.ts
+++ github-login-firebase/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { ReposComponent } from './components/repos/repos.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { UserCardComponent } from './components/user-card/user-card.component';
     HeaderComponent,
     FooterComponent,
     ReposComponent,
-    UserCardComponent
+    UserCardComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
