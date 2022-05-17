c01605caf77765f490197c5423c95ee877a90b74
Sarvang Dave <sarvang00@gmail.com>
2022-05-17 17:32:07 +0530

    Project foundation with firebase config & prereqs

---
+++ github-login-firebase/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDBX1hLFTZAjIIMT2jJNrj88tH65nAbxPk",
+    authDomain: "github-login-firebase-a2e88.firebaseapp.com",
+    projectId: "github-login-firebase-a2e88",
+    storageBucket: "github-login-firebase-a2e88.appspot.com",
+    messagingSenderId: "412323938595",
+    appId: "1:412323938595:web:9081305ef1115c60594c96"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
