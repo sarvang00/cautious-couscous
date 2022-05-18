bf7fb81d0ac89b9093bf39f83a3965eb572a10b3
Sarvang Dave <sarvang00@gmail.com>
2022-05-18 16:15:05 +0530

    Business logic of Header

--- github-login-firebase/src/app/layout/header/header.component.ts
+++ github-login-firebase/src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  email: string = '';
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    auth.getUser().subscribe((user) => {
+      this.email = user?.email!;
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  async handleSignOut() {
+    try {
+      const res = await this.auth.signOut();
+      this.router.navigateByUrl('/signin');
+      this.toastr.info("Login to use the service")
+      this.email = '';
+    } catch (error) {
+      this.toastr.error("Something is wrong")
+    }
   }
 
 }
