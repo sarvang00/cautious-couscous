69183ead8a9494b5b936c2aff18b96b3a51e5e8d
Sarvang Dave <sarvang00@gmail.com>
2022-05-23 23:30:05 +0530

    Header and Footer setup

--- travelgram/src/app/layout/header/header.component.ts
+++ travelgram/src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  email: string = '';
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { 
+    auth.getUser().subscribe((user) => {
+      console.log("User is: ", user)
+      this.email = user?.email!;
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  async handleSignOut() {
+    try {
+      await this.auth.signOut();
+      this.router.navigateByUrl("/signin");
+      this.toastr.info("User logged out successfully")
+      this.email=''
+    } catch (error) {
+      this.toastr.error("Problem in signout")
+    }
   }
 
 }
