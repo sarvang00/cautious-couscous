6e0d6a779ec967c9857fece933e7b7b2b8b9538e
Sarvang Dave <sarvang00@gmail.com>
2022-05-23 19:06:37 +0530

    Setup for the social media app

---
+++ travelgram/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ travelgram/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'social-media-74b4e',
+    appId: '1:258215262656:web:687a7370b130486f31b025',
+    databaseURL: 'https://social-media-74b4e-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'social-media-74b4e.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyB4rOq287CW68VjuIRd-1f5-jpM_b_KHRs',
+    authDomain: 'social-media-74b4e.firebaseapp.com',
+    messagingSenderId: '258215262656',
+  },
+  production: true
+};
