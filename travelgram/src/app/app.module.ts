5538266533bca3cce5fcf12ed2f4d5e9200f1e23
Sarvang Dave <sarvang00@gmail.com>
2022-05-23 22:32:22 +0530

    Generating components and services we will need

--- travelgram/src/app/app.module.ts
+++ travelgram/src/app/app.module.ts
@@ -8,10 +8,28 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { PostComponent } from './components/post/post.component';
+import { UserComponent } from './components/user/user.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddpostComponent } from './pages/addpost/addpost.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    UserComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AddpostComponent,
+    PagenotfoundComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
