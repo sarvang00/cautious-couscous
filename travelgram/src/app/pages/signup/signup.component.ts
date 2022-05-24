a752dc839373a8f25818846a1bd998839bb51b92
Sarvang Dave <sarvang00@gmail.com>
2022-05-24 15:25:19 +0530

    This package seems wrong

--- travelgram/src/app/pages/signup/signup.component.ts
+++ travelgram/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+import { NgForm } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+import { AngularFireStorage } from "@angular/fire/compat/storage";
+import { AngularFireDatabase } from "@angular/fire/compat/database";
+
+import { readAndCompressImage } from "browser-image-resizer";
 
 @Component({
   selector: 'app-signup',
