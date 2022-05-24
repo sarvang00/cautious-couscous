0516bf6f3c9452429010a21ea0b9efe4c60ef76a
Sarvang Dave <sarvang00@gmail.com>
2022-05-24 22:13:11 +0530

    starting signup. upload module remaining

--- travelgram/src/app/pages/signup/signup.component.ts
+++ travelgram/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
@@ -9,7 +9,8 @@ import { finalize } from 'rxjs/operators';
 import { AngularFireStorage } from "@angular/fire/compat/storage";
 import { AngularFireDatabase } from "@angular/fire/compat/database";
 
-import { readAndCompressImage } from "browser-image-resizer";
+// using something for image compression
+import { NgxImageCompressService } from "ngx-image-compress";
 
 @Component({
   selector: 'app-signup',
@@ -17,10 +18,45 @@ import { readAndCompressImage } from "browser-image-resizer";
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  picture: string = "https://learnyst.s3.amazonaws.com/assets/schools/2410/resources/images/logo_lco_t17sd.png"
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private db: AngularFireDatabase,
+    private storage: AngularFireStorage,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    const {email, password, username, country, bio, name} = f.form.value;
+
+    this.auth.signUp(email, password)
+    .then((res)=>{
+      console.log(res);
+      const {uid} = res.user!
+      this.db.object(`/users/${uid}`)
+      .set({
+        id: uid,
+        name: name,
+        email: email,
+        instaUserName: username,
+        country: country,
+        bio: bio,
+        picture: this.picture
+      })
+    })
+    .then(()=>{
+      this.router.navigateByUrl('/')
+      this.toastr.success("Signup success")
+    })
+    .catch((err)=>{
+      this.toastr.error("Signup failed")
+      console.log(err)
+    })
   }
 
 }
